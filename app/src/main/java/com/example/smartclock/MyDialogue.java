53546f858f9e683fb1ca3ecc8bad2fb2f7dba20c
Vaasuawesome7 <55234743+Vaasuawesome7@users.noreply.github.com>
2020-08-25 23:05:03 +0530

    1.2.3

--- app/src/main/java/com/example/smartclock/MyDialogue.java
+++ app/src/main/java/com/example/smartclock/MyDialogue.java
@@ -4,10 +4,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.media.AudioAttributes;
-import android.media.AudioManager;
-import android.media.SoundPool;
-import android.os.Build;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
--- app/src/main/java/com/example/smartclock/StartScreen.java
+++ app/src/main/java/com/example/smartclock/StartScreen.java
@@ -1,20 +1,34 @@
 package com.example.smartclock;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
-public class startScreen extends AppCompatActivity {
+public class StartScreen extends AppCompatActivity {
+
+    private SoundPool mSoundPool;
+    private int mInteract;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        setSoundPool();
+
+        ActionBar a = getSupportActionBar();
+        assert a != null;
+        a.hide();
 
         TextView smart = findViewById(R.id.smart);
         TextView clock = findViewById(R.id.clock);
@@ -33,8 +47,28 @@ public class startScreen extends AppCompatActivity {
     }
 
     public void start(View view) {
+        mSoundPool.play(mInteract, 1, 1, 0, 0, 1);
         startActivity(new Intent(this, SmartTimer.class));
         overridePendingTransition(R.anim.fadein, R.anim.fadeout);
         finish();
     }
+
+    private void setSoundPool() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MOVIE)
+                    .build();
+            mSoundPool = new SoundPool.Builder()
+                    .setMaxStreams(7)
+                    .setAudioAttributes(audioAttributes)
+                    .build();
+        }
+        else {
+            mSoundPool = new SoundPool(7, AudioManager.STREAM_MUSIC, 0);
+        }
+
+        mInteract = mSoundPool.load(this, R.raw.button, 1);
+    }
+
 }
